13c051803d80ff2ec77b342e765fc58ff8baf320
Shiikharr <83510850+Shiikharr@users.noreply.github.com>
2022-04-24 19:11:29 +0530

    Finalizing the APP

--- app/src/main/java/com/example/socialnetwork/FindFriends.java
+++ app/src/main/java/com/example/socialnetwork/FindFriends.java
@@ -1,5 +1,6 @@
 package com.example.socialnetwork;
 
+
 public class FindFriends
 {
     public String profileimage, fullname, status;
@@ -14,6 +15,7 @@ public class FindFriends
         this.fullname = fullname;
         this.status = status;
     }
+
 
     public String getProfileimage() {
         return profileimage;
